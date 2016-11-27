41355fb3a94043d55821c4f09c1360fc5049821e
Amgad <amgad@peerialism.com>
2016-11-27 11:54:20 +0200

    added julia interface

--- lib_julia/include/julia_interface.h
+++ lib_julia/include/julia_interface.h
@@ -1,8 +1,14 @@
 #pragma once
+#include "lib_algorithms.h"
+#include "lib_data.h"
+#include "lib_ensembles.h"
+#include "lib_models.h"
+#include "lib_parsing.h"
 
-namespace lib_julia {
-class DLLExport JuliaInterface {
- public:
- private:
-};
-}
+
+extern "C" float** experiment_1(char* data,int kNrTrees,int kMaxDepth,int kAlgoType,
+ bool kBagging);
+
+extern "C" float** experiment_2(char* data1,char* data2,int kNrTrees,int kMaxDepth,int kAlgoType,
+ bool kBagging);
+
