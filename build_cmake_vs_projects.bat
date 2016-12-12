fceb8d10bd1744f9a20de03aa38fa1d19fd496f6
KarlJansson <karl.jansson@hb.se>
2016-12-12 13:37:45 +0100

    - fixed tbb build script issues

--- build_cmake_vs_projects.bat
+++ build_cmake_vs_projects.bat
@@ -1,4 +1,4 @@
 rem __project_files_cmake_win64__
 mkdir __project_files_cmake_win64__& pushd __project_files_cmake_win64__
-cmake.exe -DTBB_INCLUDE_DIR="D:/API/tbb/include" -DTBB_LIBRARY_DIR="D:/API/tbb/lib/intel64/vc14" -DTBB_LIBRARY_NAME="tbb.lib" -G "Visual Studio 14 2015 Win64" ../
+cmake.exe -DTBB_INCLUDE_DIR="D:/API/tbb/include" -DTBB_LIBRARY_DIR="D:/API/tbb/lib/intel64/vc14" -DTBB_LIBRARY_NAME=".lib" -G "Visual Studio 14 2015 Win64" ../
 popd
